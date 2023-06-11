0f09edcb5fedae82243df53b317b4c44f16a501a
John Hamilton <jrhtcg@gmail.com>
2023-06-11 12:16:48 -0700

    Delete bin directory

--- bin/Debug/net7.0/scripts/runserver.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-export DOTNET_ROOT=/home/ubuntu/.dotnet
-export PATH=$PATH:$DOTNET_ROOT
-cd /home/ubuntu/server
-#sudo setcap CAP_NET_BIND_SERVICE=+eip Wampage.Server #this is now done in Wampage.service
-./WFServer --urls "http://0.0.0.0:80;https://0.0.0.0:443" 
